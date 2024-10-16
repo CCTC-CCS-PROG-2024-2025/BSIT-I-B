17903fabb9eeb897d3366df2c7dfcad95d8acdaf
Earl John H. Entero <earlentero@gmail.com>
2024-10-16 10:20:31 +0800

    Add files via upload

---
+++ Prelim/Activity III/ENTERO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class ENTERO_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        
+        String name = "Earl John Entero" ;
+        int age = 18 ; 
+        char initial = 'H' ;
+        double height = 5.0 ;
+        boolean question = true ;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(initial);
+        System.out.println(height);
+        System.out.println(question);
+    }
+}
