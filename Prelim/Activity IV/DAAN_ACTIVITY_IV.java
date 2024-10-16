573f7bc211846788a88a8047a7e4f2f36c71a80a
sundyyy466 <daansundy7@gmail.com>
2024-10-16 10:10:14 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DAAN_ACTIVITY_IV.java
@@ -0,0 +1,31 @@
+
+class DAAN_ACTIVITY_IV {
+     public static void main (String[] args ){
+
+
+    String firstname = "Sundy";
+    String lastname = "Daan";
+
+     String fullname = firstname + " " + lastname;
+     System.out.println(fullname);
+
+     int age = 18;
+     System.out.println(age); 
+     
+
+    int a = 8;
+    int b = 5;
+    int c = 3; 
+
+    int sum = a+b;
+    int diff = a-c;
+    int  prod = b*c;
+    int qout =  a/c;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(qout);
+}
+    
+}
