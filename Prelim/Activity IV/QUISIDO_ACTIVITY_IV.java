a6fb2abd0c8bf8fd0987e3a83f63e89168634c28
Ashley Nicole E. Quisido <quisidoashley22@gmail.com>
2024-10-16 10:11:23 +0800

    Add files via upload

---
+++ Prelim/Activity IV/QUISIDO_ACTIVITY_IV.java
@@ -0,0 +1,32 @@
+class QUISIDO_ACTIVITY_IV {
+    public static void main (String[]args){
+        String firstname = "Ashley Nicole";
+        String lastname = "Quisido";
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(fullname);
+        
+        int a = 4;
+        int b = 3;
+        int c = 2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+        int sum = a + b;
+        int diff = b - c;
+        int prod = a * c;
+        int quot = b / c;
+        
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+
+    }
+}
