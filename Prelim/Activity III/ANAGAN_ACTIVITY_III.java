4c998f8fa2edb509250a78e56aae91dd362a1b1b
Kiven F. Aganan <kivenaganan5@gmail.com>
2024-10-16 10:09:28 +0800

    Add files via upload

---
+++ Prelim/Activity III/ANAGAN_ACTIVITY_III.java
@@ -0,0 +1,27 @@
+ class ANAGAN_ACTIVITY_III {
+    public static void main(String[] args) {
+    String name = "AGANAN";
+    int age = 18;
+    double height = 1.5;
+    char letter = 'A';
+    boolean isopen = true;
+    
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(letter);
+    System.out.println(isopen); 
+     
+
+    
+
+
+
+
+
+
+        
+    }
+
+}
+
