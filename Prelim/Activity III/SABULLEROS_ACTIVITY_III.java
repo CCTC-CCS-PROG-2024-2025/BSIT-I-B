2b9d9a628fbf10810c54d56596438be7ab2b7d7f
Bencent C. Sabulleros <bencesab05@gmail.com>
2024-10-16 10:06:23 +0800

    Add files via upload

---
+++ Prelim/Activity III/SABULLEROS_ACTIVITY_III.java
@@ -0,0 +1,13 @@
+class SABULLEROS_ACTIVITY_III{
+    public static void main(String[] args) {
+        String name =  "bencent";
+        int age = 24;
+        boolean isclosed = false;
+        char letter = 'D';
+        float height = 1.50f;
+        System.out.println(name);
+        System.out.println(isclosed);
+        System.out.println(letter);
+        System.out.println(height);
+    }
+    }
