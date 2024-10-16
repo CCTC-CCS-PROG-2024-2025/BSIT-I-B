4e135e18fb16444770b40c63e70416634710b7e6
Romeo B. Gutang <romeogutang20@gmail.com>
2024-10-16 10:06:27 +0800

    Add files via upload

---
+++ Prelim/Activity III/GUTANG_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class GUTANG_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Romeo";
+        int age = 18;
+        double weight = 12.5;
+        char letter = 'M';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isOpen);
+
+    }
+}
