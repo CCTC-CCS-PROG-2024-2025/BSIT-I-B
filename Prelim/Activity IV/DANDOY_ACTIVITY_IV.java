3c5be70bbfe0a8dd6f6fa77a1f8e18a7928f85d9
Nibert Edrian C Dandoy <Nibertdandoy86@gmail.com>
2024-10-16 10:17:15 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DANDOY_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+class DANDOY_ACTVITY_IV {
+public static void main(String[] args) {
+    int b=5;
+int c=8;
+int d=9;
+
+int sum=b+c;
+int diff=b-c;
+int product=b*d;
+int quotient=b/c;
+
+System.out.println(sum);
+System.out.println(diff);
+System.out.println(product);
+System.out.println(quotient);
+}
+
+
+}
