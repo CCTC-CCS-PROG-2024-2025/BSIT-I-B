25fe84a38cec6971d908888aa40f85917d29b738
Trixie Jean P. Delator <xiedelator@gmail.com>
2024-10-16 10:11:42 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DELATOR_ACTIVITY_IV.java
@@ -0,0 +1,27 @@
+public class DELATOR_ACTIVITY_IV {
+    public static void main (String [] args){
+        String firstname = "Trixie Jean";
+        String lastname = "Delator";
+
+        String fullname = firstname + " " + lastname ;
+
+        System.out.println(fullname);
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b;
+        int diff = b - c;
+        int prod = a * c;
+        int result = b % c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(result);
+
+
+    
+    }
+}
