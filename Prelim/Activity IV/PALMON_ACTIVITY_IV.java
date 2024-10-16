f0fd042a83933ca8a642554745f95c4289ec93dc
Kenshen 0. Palmon <kenshenpalmon579@gmail.com>
2024-10-16 10:07:38 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PALMON_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+/**
+ * PALMON_ACTIVITY_IV
+ */
+public class PALMON_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b;
+        int diff = b - c;
+        int pod = c * a;
+
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(pod);
+    
+}
