22c3af3b03ff3ff39f550d3083c073fff07a86d3
Jann Paul C. Casia <jannpaulcasia2@gmail.com>
2024-10-16 10:19:30 +0800

    Add files via upload

---
+++ Prelim/Activity VI/CASIA_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class CASIA_ACTIVITY_VI {
+     public static void main(String[] args){
+        Scanner jannpaul = new Scanner(System.in);
+        System.out.println(x:"Enter the first number");
+        int d = jannpaul.nextInt();
+        System.out.println(x:"Enter the first number");
+        int e = jannpaul.nextInt();
+
+        int sum = d + e;
+        int diff = d - e;
+        int prod = d * e;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+    } 
+}
