ef9bf4d2219fed554d484d069f9cf91a6fa5d443
MikaelaAngeliBagata <mikaelaangelibagata@gmail.com>
2024-10-16 10:07:57 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BAGATA_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+public class BAGATA_ACTIVITY_IV {
+    public static void main(String[] args){
+        
+        int a = 4;
+        int b = 3;
+        int c = 6;
+        
+        int add = c + a;
+        int sub = a - b;
+        int mul = c * b;
+        int div = c / b;
+    
+       System.out.println(add);
+       System.out.println(sub);
+       System.out.println(mul);
+       System.out.println(div);
+    
+      String firstname = "Mikaela Angeli";
+      String lastname = "Bagata " ;
+      int age = 18;
+      double hight = 5.1;
+      float weight = 45.10f;
+      boolean ismale = false;
+      char bloodtype = 'O';
+
+     System.out.println(lastname + firstname);
+    }
+}
+
