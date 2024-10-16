e1a2b7818a9fbfe7f874b9cc6e7fc50b91c2811d
Gabriel Abañera <gabrielabanera5@gmail.com>
2024-10-16 10:09:57 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ABANERA_ACTIVITY_IV.java
@@ -0,0 +1,30 @@
+class ABANERA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int g = 18;
+        int a = 19;
+        int b = 20;
+
+        int pogi = g + a;
+        int gwapo = g - b;
+        int handsome = a * g;
+        int gabriel = a / b;
+
+        String firstname = "Gabriel";
+        String lastname = "Abañera";
+        String fullname = lastname + " " + firstname;
+        int age = 18;
+        double height = 161.69;
+        char letter = 'G';
+        Boolean isOpen = true;
+
+        System.out.println(pogi);
+        System.out.println(gwapo);
+        System.out.println(handsome);
+        System.out.println(gabriel);
+        System.out.println(fullname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
