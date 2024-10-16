fd993f4c4ff3286a85bbb08a0f2efd57df57dbd9
Audey Amir C. Agbay <audeyamirjoestar@gmail.com>
2024-10-16 10:11:45 +0800

    Add files via upload

---
+++ Prelim/Activity IV/AGBAY_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+public class AGBAY_ACTIVITY_IV{
+    public static void main(string[]args){
+        int oldest = 27;
+        int youngest = 17;
+    double weight = 93.0;
+    double height = 164.2;
+    int sum = oldest+youngest;
+    int diff = oldest-youngest;
+    double quot = height/youngest;
+    double prod = weight*youngest;
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(quot);
+    System.out.println(prod);
+    }
+}   
+
+
+
