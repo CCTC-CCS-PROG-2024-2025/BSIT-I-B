c05d43af0fc0cd86bacb5f17b33e9b6ccbd7d18b
Kyth Angelie S. Sauro <saurokyth@gmail.com>
2024-10-16 10:07:30 +0800

    Add files via upload

---
+++ Prelim/Activity III/SAURO_ACTIVITY_III.java
@@ -0,0 +1,21 @@
+ssclass SAURO_ACTIVITY_III {
+    public static void main(String []args){
+        String name="Kyth";
+        int age= 18;
+        boolean isOpen= false;
+        char letter= 'K';
+        double height= 1.2;
+        
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(isOpen);
+      System.out.println(letter);
+      System.out.println(height); 
+} 
+   
+
+ }
+
+
+
+
