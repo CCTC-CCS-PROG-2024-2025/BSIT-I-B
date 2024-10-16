3d5d1c05f70b861c15f8412034f8df80028905ab
Brian Loreto Q. Hubahib <hubahibbri@gmail.com>
2024-10-16 10:06:03 +0800

    Add files via upload

---
+++ Prelim/Activity III/HUBAHIB_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+/**
+ * HUBAHIB_ACTIVITY_III
+ */
+public class HUBAHIB_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Brian";
+        int age = 18;
+        double height = 13.3;
+        char letter = 'B';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+        //well done!
+    }    
+}
