c66dfd44243483cdc02c3c7f97c618cd59a04c26
Joshua E Canillo <joshuazcanillo@gmail.com>
2024-10-16 10:08:12 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CANILLO_ACTIVITY_IV.java
@@ -0,0 +1,27 @@
+class CANILLO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 3;
+
+        int sum = a + b;
+        int diff = a - c;
+        int prod = a * c;
+        int quot = b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+        
+        String firstname = "Joshua";
+        String lastname = "Canillo";
+        String fullname = firstname +" "+ lastname;
+        
+  
+        System.out.print(fullname);
+        
+    }
+
+}
+    
