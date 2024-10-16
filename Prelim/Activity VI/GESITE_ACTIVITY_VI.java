90ebab962b0a95efa17e2951edec9895647c95bf
Czelle Adrienne Gesite <Pueblosoliver050105@gmail.com>
2024-10-16 10:10:00 +0800

    Add files via upload

---
+++ Prelim/Activity VI/GESITE_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GESITE_ACTIVITY_VI{
+    public static void main (String[]args){
+        Scanner shiid = new Scanner (System.in);
+        System.out.println("enter the first number:");
+        int c =  shiid.nextInt();
+        System.out.println("enter the second number:");
+        int z =  shiid.nextInt();
+        
+        int hoy = c + z;
+        int hey = c - z;
+        int hi = c * z;
+        System.out.println(hoy);
+        System.out.println(hey);
+        System.out.println(hi);
+    }
+}
