e9275c61d35abfb15655fb05b8d74da415a42935
Gabriel Abañera <gabrielabanera5@gmail.com>
2024-10-16 10:11:39 +0800

    Add files via upload

---
+++ Prelim/Activity VI/ABANERA_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ABANERA_ACTIVITY_VI {
+    public static void main(String[] args) {
+    Scanner ily = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    int num1 = ily.nextInt ();
+    System.out.println("Enter the second number; ");
+    int num2 = ily.nextInt();
+
+    int gab = num1 + num2;
+    int riel = num1 - num2;
+    int gwapo = num1 * num2;
+
+    System.out.println(gab);
+    System.out.println(riel);
+    System.out.println(gwapo);
+    }
+}
