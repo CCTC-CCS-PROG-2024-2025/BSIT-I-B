599fefdbac461463c8c34dd595e3c9aca71907ac
Jemima T. Elemino <maijem840@gmail.com>
2024-10-16 10:11:57 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ELEMINO_ACTIVITY_IV .java
@@ -0,0 +1,44 @@
+class GELSANO_ACTIVITY_IV {
+    public static void main (String[] args) {
+
+        String Firstname = "Jemima";
+        String Lastname = " Elemino ";
+        String Fullname = Firstname + Lastname;
+
+
+        int Age = 19;
+        char Gender = 'F';
+        double Height = 167.67;
+
+        String Mix = Fullname + Age;
+
+        int G = 31;
+        int K = 29;
+        int L = 18;
+
+        int Sum = G + K;
+        int Diff = K - L;
+        int prod = L * G;
+        int quot = K / L;
+
+        Boolean Isopen = true;
+
+        System.out.println(Mix);
+        System.out.println(Fullname);
+        System.out.println(Age);
+        System.out.println(Gender);
+        System.out.println(Height);
+        System.out.println(Sum);
+        System.out.println(Diff);
+        System.out.println(prod);
+        System.out.println(quot);
+        System.out.println(Isopen);
+
+
+
+
+    
+
+    
+    }
+}
