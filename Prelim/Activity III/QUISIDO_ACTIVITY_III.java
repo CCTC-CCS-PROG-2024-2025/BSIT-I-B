2ce9ab47a900be107b5a226b81517984fc055407
Ashley Nicole E. Quisido <quisidoashley22@gmail.com>
2024-10-16 10:10:54 +0800

    Add files via upload

---
+++ Prelim/Activity III/QUISIDO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class QUISIDO_ACTIVITY_III{
+    public static void main (String[] args){
+        String name = "ASHLEY";
+        int age = 18;
+        double height = 1.62;
+        char letter = 'A';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+
+    }
+}
