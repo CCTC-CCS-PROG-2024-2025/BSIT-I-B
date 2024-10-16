188a9b46408063af1ce1339d1e3bb8a8d48648e4
Christian Y. Sanchez Jr. <christianjr1018@gmail.com>
2024-10-16 10:06:04 +0800

    Add files via upload

---
+++ Prelim/Activity III/SANCHEZ_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class SANCHEZ_ACTIVITY_III {
+  public static void main(String[] args) {
+    String name = "Christian";
+    int age = 18;
+    double height = 168.2;
+    char letter = 'B';
+    boolean isOpen = true;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(letter);
+    System.out.println(isOpen);
+
+  }
+}
