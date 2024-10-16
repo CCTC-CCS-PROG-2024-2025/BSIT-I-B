43306ab5c2030db1d7f7cb9943b35e62fed31f56
Miko Yankin <micoyankin1@gmail.com>
2024-10-16 10:08:39 +0800

    Add files via upload

---
+++ Prelim/Activity IV/YANKIN_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+ class YANKIN_ACTIVITY_IV {      
+    
+        public static void main(String[]args){
+        int b=5;
+        int c=8;
+        int d=9;
+
+        int sum=b+c;
+        int diff=b-c;
+        int proud=b*d;
+        int quot=b/c; 
+
+
+
+   System.out.println(sum);
+   System.out.println(diff); 
+   System.out.println(proud);
+   System.out.println(quot);
+ }
+}
