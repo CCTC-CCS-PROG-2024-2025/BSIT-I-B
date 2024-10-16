6ebe7192bdee896dc8074fce12741ae6c76ea9e7
Dean Mark A. Salapi <salapidean@gmail.com>
2024-10-16 10:09:51 +0800

    Add files via upload

---
+++ Prelim/Activity III/SALAPI_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class SALAPI_ACTIVITY_III {
+    public static void main(String[] args){
+        
+        String name = "Dean Mark Salapi" ;
+        int age = 18 ;
+        char initial = 'S' ;
+        double height = 5.2 ;
+        boolean question = true ;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(initial);
+        System.out.println(height);
+        System.out.println(question);
+    }
+}
