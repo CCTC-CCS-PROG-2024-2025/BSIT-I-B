fd997d7fe39ae02c4dc95bf1da3f5e79849307a6
Czelle Adrienne Gesite <Pueblosoliver050105@gmail.com>
2024-10-16 10:09:14 +0800

    Add files via upload

---
+++ Prelim/Activity IV/GESITE_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+class GESITE_ACTIVITY_IV{
+    public static void main (String[]args){
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        double wawa = 162;
+        int haha = a+b;
+        int hehe = a-b;
+        int batignawng =a/b;
+        int gwapa = a*c;
+        double rara = a+wawa;
+        System.out.println(haha);
+        System.out.println(hehe);
+        System.out.println(batignawng);
+        System.out.println(gwapa);
+        System.out.println(rara);
+    }
+}
