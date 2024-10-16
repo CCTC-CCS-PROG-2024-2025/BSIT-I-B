40b25f5d35f448c1bf3656de35611a4c000f6d38
Delmar Narvasa <delmarnarvasa09@gmail.com>
2024-10-16 10:12:35 +0800

    Add files via upload

---
+++ Prelim/Activity VI/NARVASA_ACTIVITY_VI.java.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class NARVASA_ACTIVITY_VI {
+    public static void main (String[] args) {
+        Scanner kajjo = new Scanner (System.in);
+        System.out.println("Enter the first number :");
+        int t = kajjo.nextInt();
+        System.out.println("Enter the second number: ");
+        int a = kajjo.nextInt();
+
+        int sum = t + a;
+        int diff = t - a;
+        int jo = t * a;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(jo);
+    }
+}
+
