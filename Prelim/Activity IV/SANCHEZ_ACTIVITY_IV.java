119ef9fe21121825bbdee6878a41255b12b16d4e
Christian Y. Sanchez Jr. <christianjr1018@gmail.com>
2024-10-16 10:09:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SANCHEZ_ACTIVITY_IV.java
@@ -0,0 +1,34 @@
+class SANCHEZ_ACTIVITY_IV {
+  public static void main(String[] args) {
+    int age=18;
+    int bday=16;
+    int SY=2024;
+    int d=age+bday;
+    double e=12.23;
+    double f=10.77;
+    String firstname= "Christian";
+    String lastname= "Sanchez";
+    String val= 10 + 10 + "Christian";
+
+
+    int sum= age+bday;
+    int diff= bday-SY;
+    int prod= age*SY;
+    int qout= bday/SY;
+    int difference=d-SY;
+    double summ=e+f;
+    double dif=e-f;
+    String fullname= firstname + " " + lastname;
+    String value= val + " " + lastname;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(qout);
+    System.out.println(difference);
+    System.out.println(summ);
+    System.out.println(dif);
+    System.out.println(fullname);
+    System.out.println(value);
+    }
+}
