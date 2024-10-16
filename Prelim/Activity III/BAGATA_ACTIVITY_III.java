2c9a1254814cf69a6b1c88f9450cdbdcb201d7a8
MikaelaAngeliBagata <mikaelaangelibagata@gmail.com>
2024-10-16 10:06:15 +0800

    Add files via upload

---
+++ Prelim/Activity III/BAGATA_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class BAGATA_ACTIVITY_III {
+    public static void main(String[] agrs) {
+        String name = "Mikaela";
+        int age = 18;
+        double hight = 5.1;
+        float wight = 48.4f;
+        boolean isfemale = true;
+        char bloodtype = 'O';
+
+     System.out.println(name); 
+     System.out.println(age);
+     System.out.println(hight);
+     System.out.println(wight);
+     System.out.println(isfemale);
+     System.out.println(bloodtype);
+    }
+}
