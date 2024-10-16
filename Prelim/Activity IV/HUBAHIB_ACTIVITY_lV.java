1ceb405c7bf7244f6f6b0aac861b3f2d39a88b74
Brian Loreto Q. Hubahib <hubahibbri@gmail.com>
2024-10-16 10:07:58 +0800

    Add files via upload

---
+++ Prelim/Activity IV/HUBAHIB_ACTIVITY_lV.java
@@ -0,0 +1,20 @@
+public class HUBAHIB_ACTIVITY_lV {
+    public static void main(String[] args) {
+      double a = 9.3;
+      double b = 6.3;
+      double c = 3.2;
+
+      double sum = a+b;
+      double diff = b-c;
+      double prod = c*b;
+      double quotient = c/a;
+
+      System.out.println(sum);
+      System.out.println(diff);
+      System.out.println(prod);
+      System.out.println(quotient);
+      //well done!
+        
+
+    }
+}
