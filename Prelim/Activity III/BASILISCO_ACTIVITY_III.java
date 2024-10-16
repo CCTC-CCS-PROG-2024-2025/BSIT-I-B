3a5fd67bcb8f5499394a5eb1441246de6036d407
Neil John C. Basilisco <neilbasilisco158@gmail.com>
2024-10-16 10:06:24 +0800

    Add files via upload

---
+++ Prelim/Activity III/BASILISCO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class BASILISCO_ACTIVITY_III{
+    public static void main(String[]args){
+        String name = "Basilisco";
+        int age = 18;
+        double height = 162.52;
+        char letter = 'C';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+
+    }
+}
+ 
