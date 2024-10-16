1ce54bf59bfe4f2557550cd0dcb9445708e4f4cc
Sigfred O. Pardillo <pardillosigfred@gmail.com>
2024-10-16 10:10:24 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PARDILLO_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+public class PARDILLO_ACTIVITY_IV {
+    public static void main(String[]args){
+        int a =10;
+        int b =15;
+        int c =20;
+        int sum = a + b;
+        int diff = b - c;
+        int prod = a * c;
+        int quot = b / c;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
