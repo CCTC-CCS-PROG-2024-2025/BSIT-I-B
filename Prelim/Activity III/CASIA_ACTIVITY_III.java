a8a0eb274f59cccec5b753d663ed50cd937d4c80
Jann Paul C. Casia <jannpaulcasia2@gmail.com>
2024-10-16 10:12:18 +0800

    Add files via upload

---
+++ Prelim/Activity III/CASIA_ACTIVITY_III.java
@@ -0,0 +1,21 @@
+class CASIA_ACTIVITY_III {
+ public static void main(Casia[] args) {
+    String name ="JannPaul";
+    int age = 19;
+   double height = 175.26;
+   char gender = 'C';
+   boolean isStudent = true;
+   
+   System.out.println(name);
+   System.out.printl(age);
+   System.out.println(height);
+   System.out.println(letter);
+   System.stem.println(isOpen);
+   }
+}
+
+ 
+
+
+    
+
