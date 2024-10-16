c7530e82aa24bdb8fc3d0d1ab3508e847ab139c9
Stephanie Ninal <steffyninal23@gmail.com>
2024-10-16 10:06:49 +0800

    Add files via upload

---
+++ NIÑAL_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+public class NIÑAL_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Stephanie";
+        int age = 18;
+        double hieght = 1.4;
+        char letter = 'A';
+        Boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hieght);
+        System.out.println(letter);
+        System.out.println(isOpen);
+
+    }
+}
