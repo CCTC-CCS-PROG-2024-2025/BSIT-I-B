56e530f22962e7bed312f258cfa3a88f7c8f646c
Michelle Villarante <villarantemichelle35@gmail.com>
2024-10-16 10:06:18 +0800

    Add files via upload

---
+++ Prelim/Activity III/VILLARANTE_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class VILLARANTE_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Michelle";
+        int age = 19;
+        double hight = 5.1;
+        float wight = 49.6f;
+        boolean isfemale = true;
+        char bloodtype = 'A';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hight);
+        System.out.println(wight);
+        System.out.println(isfemale);
+        System.out.println(bloodtype);
+
+    }
+}
