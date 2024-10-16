27aed571e151da3132b81b2afa464d5bf9994036
Paul Angelo A. Magbanua <paulangelomagbanua1@gmail.com>
2024-10-16 10:07:45 +0800

    Add files via upload

---
+++ Prelim/Activity III/MAGBANUA_ACTIVITY_III.java
@@ -0,0 +1,34 @@
+class MAGBANUA_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Paul";
+        int age = 18;
+        double height = 5.6;
+        float number = 2.3f;
+        char blood = 'O';
+        boolean question = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(number);
+        System.out.println(blood);
+        System.out.println(question);
+    }
+}
+
+
+
+
+
+
+        
+
+    
+
+
+
+
+
+
+        
+    
