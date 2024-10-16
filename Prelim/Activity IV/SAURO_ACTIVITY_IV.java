294da54715c01b68f77f5c3aa9cf55961009b0bc
Kyth Angelie S. Sauro <saurokyth@gmail.com>
2024-10-16 10:09:47 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SAURO_ACTIVITY_IV.java
@@ -0,0 +1,28 @@
+public class SAURO_ACTIVITY_IV {
+    public static void main(String[] args) {
+
+        String firstname = "Kyth ";
+        String lastname = "Sauro ";
+
+        String fullname = firstname + lastname;
+        String section = "BSIT-1B";
+
+
+        int x = 12;
+        int y =3;
+        int b = 4;
+
+        int sum = x + y;
+        int qout = x / b;
+        int diff = b- y;
+        int prod = x * y;
+
+        System.out.println(fullname);
+        System.out.println(section);
+        System.out.println(sum);
+        System.out.println(qout);
+        System.out.println(diff);
+        System.out.println(prod);
+
+    }
+}
