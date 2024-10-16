8858114c92c8378e67bc2d6164a197e1dd844cd4
Jessa Mae F. Lugay <jessamaefernandezlugay@gmail.com>
2024-10-16 10:11:26 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LUGAY_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+public class LUGAY_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String firstname = "Jessa";
+        String lastname = "Lugay";
+        String fullname = firstname + lastname;
+
+        System.out.println(fullname);
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b;
+        int diff = b - c;
+        int prod = a * c;
+        int quot = b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+    }
+}
