2875e10a8c3ff924cbeb8da9884c7030387539a9
Bencent C. Sabulleros <bencesab05@gmail.com>
2024-10-16 10:08:09 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SABULLEROS_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+public class SABULLEROS_ACTIVITY_IV{
+    public static void main(String[]args){
+        int eldest = 24;
+        int youngest = 17;
+    double weight = 50.5;
+    double height = 172.2;
+    int sum = eldest+youngest;
+    int diff = eldest-youngest;
+    double quot = height/youngest;
+    double prod = weight*youngest;
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(quot);
+    System.out.println(prod);
+    }
+}
