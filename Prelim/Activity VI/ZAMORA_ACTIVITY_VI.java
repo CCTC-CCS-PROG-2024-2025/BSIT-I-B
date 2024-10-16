d060b5ae70844b388f7c77f40f2f1d691777d6c4
Grace Adeline Zamora <graceadelinezamora@gmail.com>
2024-10-16 10:09:25 +0800

    Add files via upload

---
+++ Prelim/Activity VI/ZAMORA_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class ZAMORA_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+  
+    }
+}
