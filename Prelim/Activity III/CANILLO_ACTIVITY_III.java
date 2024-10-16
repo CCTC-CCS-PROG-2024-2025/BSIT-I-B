83fc813f4ec1c484b71ad38a880d70259c0388b9
Joshua E Canillo <joshuazcanillo@gmail.com>
2024-10-16 10:06:11 +0800

    Add files via upload

---
+++ Prelim/Activity III/CANILLO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class CANILLO_ACTIVITY_III {
+
+    public static void main(String[] args) {
+        String name = "Joshua";
+        int age = 18;
+        double height = 1.5;
+        char letter = 'J';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
