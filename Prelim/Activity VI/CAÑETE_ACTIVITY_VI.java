bbd656ed16f660378722908dd722e0267c6d0d0b
Justine Mae Canete <justinemaecanete206@gmail.com>
2024-10-16 10:11:39 +0800

    Add files via upload

---
+++ Prelim/Activity VI/CAÑETE_ACTIVITY_VI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CAÑETE_ACTIVITY_VI{
+    public static void main (String[]args) {
+        Scanner  sc = new Scanner (System.in);        
+        System.out.println( "Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the first number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+
+    }
+}
+    
+
