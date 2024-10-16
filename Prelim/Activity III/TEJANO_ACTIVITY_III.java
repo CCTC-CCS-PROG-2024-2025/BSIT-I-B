648b619c598260da58e1248100ca5167b053a96a
Fritz A j Abendan Tejano <fritzaj19@gmail.com>
2024-10-16 10:06:37 +0800

    Add files via upload

---
+++ Prelim/Activity III/TEJANO_ACTIVITY_III.java
@@ -0,0 +1,24 @@
+class FRITZ_ACTIVITY.III.{
+
+
+
+        public static void main ( String[]args) {
+            String name = " FRITZ";
+            int age = 19;
+            double heigth = 1.5;
+            char letter ='A';
+            boolean isOpen =true;
+
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(heigth);
+            System.out.println(letter);
+            System.out.println(isOpen);
+        
+        }
+    
+    }
+    
+        
+    
+
