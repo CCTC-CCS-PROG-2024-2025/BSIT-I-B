ba29632d329f5b3d78f5c4646925f391fcf35a0b
BEVERLY PERFECIO <Perfeciobeverly24@gmail.com>
2024-10-16 10:09:31 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PERFECIO_ACTIVITY_lV.java
@@ -0,0 +1,27 @@
+public class PERFECIO_ACTIVITY_lV {
+    public static void main(String[] args) {
+
+        String firstname = "Beverly ";
+        String lastname = "Perfecio";
+
+        String fullname = firstname + lastname;
+        String section =  "BSIT 1E";
+
+        int x = 8;
+        int y = 4;
+        int z = 12;
+
+        int sum = x + y;
+        int diff = y - z;
+        int prod = x * y;
+        int quot = y / z;
+
+        System.out.println(fullname);
+        System.out.println(section);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
+
