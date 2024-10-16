c6b7922fb9b6d74dbc017a9f03258905cabe0855
Jave G. Sedillo <javesedillo30@gmail.com>
2024-10-16 10:13:29 +0800

    Add files via upload

---
+++ Prelim/Activity III/SEDILLO_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class SEDILLO_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Jave";
+        int age = 19;
+        double height = 164.44;
+        char letter = 'A';
+        boolean isOpen = true;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
