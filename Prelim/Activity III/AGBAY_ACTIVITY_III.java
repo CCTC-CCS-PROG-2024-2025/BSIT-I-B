51ba6616d168ce919e40607da147b3054259b5f9
Audey Amir C. Agbay <audeyamirjoestar@gmail.com>
2024-10-16 10:06:03 +0800

    Add files via upload

---
+++ Prelim/Activity III/AGBAY_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class AGBAY_ACTIVITY_III{
+    public static void main(String[] args) {
+        String name = "Audey";
+        int age = 17;
+        boolean isclosed = false;
+        char letter = 'C';
+        float height = 1.50f;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isclosed);
+        System.out.println(letter);
+        System.out.println(height);
+    }
+}
+        
+
+
+
