dff5140bd32eb217631823ca8080dfeaa5eaccb0
Stephanie Ninal <steffyninal23@gmail.com>
2024-10-16 10:14:37 +0800

    Add files via upload

---
+++ Prelim/Activity IV/NIÑAL_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+public class NIÑAL_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String firstname = "Steffy";
+
+        int x = 8;
+        int y = 12;
+        int z = 4;
+
+        int sum = x + y;
+        int diff = y - z;
+        int prod = x * y;
+        int qout = y / z;
+        System.out.println(firstname);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qout);
+    }
+}
