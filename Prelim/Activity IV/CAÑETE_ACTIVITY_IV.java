db08600c2ce8b820175a81b107e57c4d297b4a17
Justine Mae Canete <justinemaecanete206@gmail.com>
2024-10-16 10:10:53 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAÑETE_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+public class CAÑETE_ACTIVITY_IV {
+    public static void main (String[] args) {
+
+String firstname = "Justine" ;
+String lastname = "Cañete" ;
+
+String fullname = firstname + " " + lastname ;
+
+System.out.println(fullname);
+
+        int a = 3 ;
+        int b = 6;
+        int c = 9;
+        int sum = a + b;
+        int diff = b + c;
+        int prod = a * b;
+        int result = c % a;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(result);
+
+    }
+}
