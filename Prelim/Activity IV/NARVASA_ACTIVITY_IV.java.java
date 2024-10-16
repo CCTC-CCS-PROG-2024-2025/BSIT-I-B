58432354af010cfd72dc121ccf05530980db9a78
Delmar Narvasa <delmarnarvasa09@gmail.com>
2024-10-16 10:12:19 +0800

    Add files via upload

---
+++ Prelim/Activity IV/NARVASA_ACTIVITY_IV.java.java
@@ -0,0 +1,15 @@
+class NARVASA_ACTIVITY_IV {
+    public static void main (String[]args){
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int sum = a + b;
+        int diff = a - c;
+        int pro = b * a;
+        int quotient = b / c;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(pro);
+        System.out.println(quotient);
+    }
+}
