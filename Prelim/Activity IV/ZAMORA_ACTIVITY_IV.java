1b00f08643a718aae209c16ac46cd388b90d1c50
Grace Adeline Zamora <graceadelinezamora@gmail.com>
2024-10-16 10:08:46 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ZAMORA_ACTIVITY_IV.java
@@ -0,0 +1,50 @@
+class ZAMORA_ACTIVITY_IV {
+    public static void main (String[] args) {
+        String firstname = "Grace Adeline";
+        String lastname = "Zamora";
+        String fullname = firstname + lastname;
+
+        System.out.println(fullname);
+        
+        int j = 2;
+        int k = 4;
+        int l = 6;
+
+        int sum = j + k;
+        int diff = j - l;
+        int prod = j * l;
+        int quot = k / l;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+        double price1 = 20.05;
+        double price2 = 19.06;
+        double price3 = 15.25;
+        double price4 = 20.20;
+
+        double sum2 = price1 + price2;
+        double sum3 = price2 - price4;
+        double sum4 = price1 * price3;
+        double sum5 = price3 / price4;
+
+
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+        System.out.println(sum5);
+
+        int salary = 50000;
+        int tax = 15000;
+        int result = salary - tax;
+
+        System.out.println(result);
+
+        int price = 15000;
+
+        System.out.println(price*0.3);
+
+    }
+}
