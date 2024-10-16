cfe100c39f3c754a36edafe75d35ebc95ebd9d3d
BEVERLY PERFECIO <Perfeciobeverly24@gmail.com>
2024-10-16 10:07:27 +0800

    Add files via upload

---
+++ Prelim/Activity III/PERFECIO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+ class PERFECIO_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "berly";
+        int age = 19;
+        double height = 153.5;
+        char letter = 'B';
+        boolean isOpen = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
+
+
+
+
