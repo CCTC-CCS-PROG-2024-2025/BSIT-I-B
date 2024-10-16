12bcbac1f2b3ef5263e8f72178c3f178dd676419
Michelle Villarante <villarantemichelle35@gmail.com>
2024-10-16 10:07:56 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLARANTE_ACTIVITY_IV.java
@@ -0,0 +1,32 @@
+public class VILLARANTE_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 2;
+
+        int sum = a + b;
+        int diff = a - c;
+        int pro = b * c;
+        int quet = b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(pro);
+        System.out.println(quet);
+
+        String firstname = "Michelle";
+        String lastname = "Villarante";
+        int age = 19;
+        double hight = 5.1;
+        float wight = 49.6f;
+        boolean isfemale = true;
+        char bloodtype = 'A';
+
+        System.out.println(lastname + lastname);
+        System.out.println(hight + wight);
+        System.out.println(wight - hight);
+        System.out.println(isfemale);
+        System.out.println(bloodtype);
+
+    }
+}
