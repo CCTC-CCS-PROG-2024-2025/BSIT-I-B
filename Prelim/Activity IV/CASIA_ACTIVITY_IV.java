4acd858151fb92020c1c728819036df90b2dd348
Jann Paul C. Casia <jannpaulcasia2@gmail.com>
2024-10-16 10:21:51 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CASIA_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+public class CASIA_ACTIVITY_IV {
+    public static void main (string [] args) {
+        String firstname = "Jann Paul";
+        String lastname = "Casia";
+
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(Fullname);
+        int a = 20;
+        int B = 5;
+        int c = 2;
+        int sum = a+B;
+        int difference = b-c;
+        int product = a*c;
+        int quotient = b/c;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
