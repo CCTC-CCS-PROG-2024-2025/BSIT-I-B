7a3e74a6c21c5024499f6a8639e38b79bf16be2b
sundyyy466 <daansundy7@gmail.com>
2024-10-16 10:11:34 +0800

    Add files via upload

---
+++ Prelim/Activity VI/DAAN_ACTIVITY_VI.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class DAAN_ACTIVITY {
+   public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    int num1 = sc.nextInt();
+    System.out.println("Enter the second number:");
+    int num2 = sc.nextInt();
+
+
+    int sum = num1 + num2;
+    int diff = num2 - num1;
+    int prod = num1 * num2;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+
+    
+
+
+    
+    
+   }
+
+}
