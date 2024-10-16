3d4ebc5d3eab4c10c95f0d424fea2a26f42cfcf7
Kenshen 0. Palmon <kenshenpalmon579@gmail.com>
2024-10-16 10:06:09 +0800

    Add files via upload

---
+++ Prelim/Activity III/PALMON_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class PALMON_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name ="kenshen";
+        int age = 18;
+        char letter = 'k';
+        double height = 12.1;
+        boolean isOpen = true;
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(isOpen);
+}
