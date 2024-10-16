3b4798055753411045fea5729b42733650797749
Ashley Nicole E. Quisido <quisidoashley22@gmail.com>
2024-10-16 10:12:31 +0800

    Add files via upload

---
+++ Prelim/Activity VI/QUISIDO_ACTIVITY_VI.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class QUISIDO_ACTIVITY_V {
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+
+        
+        System.out.println("enter the first number:");
+        int num1 = sc.nextInt();
+        System.out.println("enter the second number:");
+        int num2 = sc.nextInt();
+        
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+
+
+        
+    
+        
+    }
+}
+    
+
+    
