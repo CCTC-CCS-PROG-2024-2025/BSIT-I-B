e213c34184399c86bce48a0ef65c8514d11bf7b6
Fritz A j Abendan Tejano <fritzaj19@gmail.com>
2024-10-16 10:15:18 +0800

    Add files via upload

---
+++ Prelim/Activity IV/TEJANO_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+public class TEJANO_ACTIVITY_IV {
+    public static void main(String[]args){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int sum = a+b;
+    int diff = b-c;
+    int prod = a*c;
+    int quot = b/c;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(quot);
+
+    String firstname = "Fritz";
+    String lastname = "Tejano";
+
+    String fullname = firstname+" "+lastname;
+    System.out.println(fullname);
+
+    
+    
+
+
+
+    }
+}
