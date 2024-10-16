59551b43911afe1742a83716eb79c9136d71b39b
Trixie Jean P. Delator <xiedelator@gmail.com>
2024-10-16 10:10:47 +0800

    Add files via upload

---
+++ Prelim/Activity III/DELATOR_ACTIVITY_III.java
@@ -0,0 +1,22 @@
+class DELATOR_ACTIVITY_III {
+ public static void main (String[] args) {
+    String name;
+    name = "Trixie" ;
+    int age = 18;
+    float height = 3.146f;
+    double weight = 40;
+    char letter = 'T' ;
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(letter);
+
+
+
+
+
+
+
+ }
+}
