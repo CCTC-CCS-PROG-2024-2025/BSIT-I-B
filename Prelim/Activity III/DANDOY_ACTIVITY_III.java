e7ce9fc3a5c11d5351d2b40ed6bc6f6c5dbb26df
Nibert Edrian C Dandoy <Nibertdandoy86@gmail.com>
2024-10-16 10:07:06 +0800

    Add files via upload

---
+++ Prelim/Activity III/DANDOY_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+ class DANDOY_ACTIVITY_III {
+
+    public static void main(String[]args) {
+        String name = "Sheen";
+        int age = 18;
+        double heigth = 160.02;
+        char middle = 'L';
+        boolean isClose = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(heigth);
+        System.out.println(middle);
+        System.out.println(isClose);
+    }
+ }
