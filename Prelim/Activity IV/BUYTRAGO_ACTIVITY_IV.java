4a2d54af257308294ace298041cc88559d4e73a8
Zachary Ivan C. Buytrago <zacharyivanbuytrago2@gmail.com>
2024-10-16 10:08:24 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BUYTRAGO_ACTIVITY_IV.java
@@ -0,0 +1,32 @@
+class BUYTRAGO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        
+        int a = 2 ;
+        int b = 4 ;
+        int c = 6 ;
+
+        String Firstname = "Zachary Ivan" ;
+        String Lastname = " Buytrago" ;     
+
+        double d1 = 1.2 ;
+        float f1 = 2.4f ;
+        boolean human = true ;
+
+        int sum = a + b ;
+        int diff = b - c ;
+        int prod = a * b ;
+        int quot = b / c ;
+
+        String Fullname = Firstname + Lastname ;
+        double result = d1 + f1 ;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+        System.out.println(Fullname);
+        System.out.println(result);
+        System.out.println(human);
+        
+    }
+}
