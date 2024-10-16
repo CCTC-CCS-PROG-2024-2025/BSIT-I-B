8a0dd76181f32c654e5b4b571c02175ecd826b78
sundyyy466 <daansundy7@gmail.com>
2024-10-16 10:07:12 +0800

    Add files via upload

---
+++ Prelim/Activity III/DAAN_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+ class DAAN_ACTIVITY_III {
+    public static void main (String [] args) {
+        String name = "Sundy";
+        int age = 18;
+        double height = 149;
+        char letter = 'S';
+        Boolean Isopen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(Isopen);
+
+
+        
+
+    }
+    
+}
