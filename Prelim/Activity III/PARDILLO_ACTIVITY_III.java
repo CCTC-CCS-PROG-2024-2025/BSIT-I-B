ede33d6c9c865723cf027af0f8be4cdadfedba7e
Sigfred O. Pardillo <pardillosigfred@gmail.com>
2024-10-16 10:08:40 +0800

    Add files via upload

---
+++ Prelim/Activity III/PARDILLO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class SIGFRED_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "SIGFRED";
+        int age = 21;
+        double height = 72;
+        float weight = 62f;
+        char letter = 'S';
+        boolean isStudent = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(isStudent);
+        System.out.println(letter);
+    }    
+}
+
