0428476cfa44dbbc0c129146c933888fb37fca79
Zachary Ivan C. Buytrago <zacharyivanbuytrago2@gmail.com>
2024-10-16 10:06:53 +0800

    Add files via upload

---
+++ Prelim/Activity III/BUYTRAGO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class BUYTRAGO_ACTIVITY_III {
+    public static void main(String[] args) {
+        
+        String name = "Zachary Ivan Buytrago" ;
+        int age = 18 ;
+        char initial = 'C' ;
+        double height = 5.2 ;
+        boolean questions = true ;
+    
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(initial);
+        System.out.println(height);
+        System.out.println(questions);
+        
+
+    }
+}
