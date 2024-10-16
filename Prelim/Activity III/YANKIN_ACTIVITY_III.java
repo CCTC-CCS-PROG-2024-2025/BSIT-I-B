c5f8a72947fca2604f8f9c640ffcf6e0967fb677
Miko Yankin <micoyankin1@gmail.com>
2024-10-16 10:06:02 +0800

    Add files via upload

---
+++ Prelim/Activity III/YANKIN_ACTIVITY_III.java
@@ -0,0 +1,20 @@
+class YANKIN_ACTIVITY_III {
+    public static void main(String[]args) {
+
+    String name="YANKIN"; 
+    int age=20;
+    double height=1.5;
+    char letter='Y';
+    boolean isopen=true;
+
+
+    System.out.println(name); 
+    System.out.println(age);
+    System.out.println(height);
+     System.out.println(letter);
+     System.out.println(isopen);
+
+
+    }
+}
+ 
