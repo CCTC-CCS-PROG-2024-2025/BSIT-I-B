2e86af9d90f2772fbd1e6c268d07156fe7879960
Czelle Adrienne Gesite <Pueblosoliver050105@gmail.com>
2024-10-16 10:07:28 +0800

    Add files via upload

---
+++ Prelim/Activity III/GESITE_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+class GESITE_ACTIVITY_III {
+    public static void main (String[] args) {
+        String name = "Czelle";
+        int age = 18;
+        double weight = 18.24;
+        float height = 5.7f;
+        char letter = 'b';
+        boolean isopen = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isopen);
+    }
+}
