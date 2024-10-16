55d17f56d8c3366df81f6d13d3a9e044c41b4d1d
Christian DAE M. Villaro <akamekyoto157@gmail.com>
2024-10-16 10:15:41 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLARO_ACTIVITY_IV.java
@@ -0,0 +1,43 @@
+public class VILLARO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int price = 46;
+        int measure = 77;
+        int cost = 425828;
+
+        double mine = 0.601848410;
+        double lol = 702.05;
+        double shoot = 274.98;
+
+        int sum = (price + cost);
+        int diff = (cost + measure);
+        int total = (cost + measure + price);
+        int qoutient = (cost/price);
+        int negative = (price/sum);
+        int nia = (price*measure + cost/price);
+
+        double jum = (lol/mine);
+        double yum = nia + lol;
+
+        int tom = (nia*measure);
+        int sub = (diff/total);
+        int stat = (total + negative);
+        int you = (sum - nia);
+
+        System.out.println(sum);
+        System.out.println(yum);
+        System.out.println(cost);
+        System.out.println(total);
+        System.out.println(qoutient);
+        System.out.println(negative + nia);
+        System.out.println(nia - negative);
+        System.out.println(diff + yum);
+        System.out.println(tom);
+        System.out.println(sub);
+        System.out.println(stat);
+        System.out.println(you);
+
+
+
+    }
+
+}
