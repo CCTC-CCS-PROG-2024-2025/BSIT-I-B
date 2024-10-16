671cc5b15e327bec5fbf18257594c57f30697665
Holiness Ferdinand III. A. Marquez <frostnebula01@gmail.com>
2024-10-16 10:20:06 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MARQUEZ_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+public class MARQUEZ_ACTIVITY_III {
+    public static void main(String[] args) {
+    
+        String name = "REX";
+        int age = 18;
+        float height = 5.5f;
+        char hat = 'R';
+        boolean isopen = true;
+        double myNum = 19.99d;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(hat);
+        System.out.println(isopen);
+        System.out.println(myNum);
+    }
+}
