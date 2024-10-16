187c913d4ccb49c5417670422ee057e08baba4c2
Jessa Mae F. Lugay <jessamaefernandezlugay@gmail.com>
2024-10-16 10:10:44 +0800

    Add files via upload

---
+++ Prelim/Activity III/LUGAY_ACTIVITY_III.java
@@ -0,0 +1,22 @@
+class LUGAY {
+    public static void main (String[] args) {
+        String name;
+        name = "Jessa" ;
+        int age = 18;
+        float height = 3.146f;
+        double weight = 40;
+        char letter = 'J' ;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+    
+
+    }
+}
+
+
+
+    
+
