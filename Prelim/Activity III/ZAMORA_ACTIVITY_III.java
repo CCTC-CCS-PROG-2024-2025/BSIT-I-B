2824757568d3573d106e5b15171123cde56fca11
Grace Adeline Zamora <graceadelinezamora@gmail.com>
2024-10-16 10:08:01 +0800

    Add files via upload

---
+++ Prelim/Activity III/ZAMORA_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class ZAMORA_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Adeline";
+        int age = 17;
+        double height = 1.52;
+        char letter = 'A';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
