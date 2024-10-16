8093c8c0c1e9dda9f31bd040e8670bf001ac7309
Dean Mark A. Salapi <salapidean@gmail.com>
2024-10-16 10:10:20 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SALAPI_ACTIVITY_IV.java
@@ -0,0 +1,35 @@
+class SALAPI_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 4;
+        int c = 6;
+
+        String Firstname = "Dean Mark" ;
+        String Lastname = "Salapi" ;
+
+        double d1 = 1.2 ;
+        float f1 = 2.4f ;
+        boolean human = true ;
+
+        int sum = a + b ;
+        int diff = a - b ;
+        int prod = c * b ;
+        int quot = c / b ;
+
+        String Fullname = Firstname + Lastname ;
+        double result = d1 + f1;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+        System.out.println(Fullname);
+        System.out.println(result);
+        System.out.println(human);
+        
+        
+    }
+
+}
