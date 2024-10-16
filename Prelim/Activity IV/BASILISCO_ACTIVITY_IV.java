1131fff9a02e75c26f01764131d83c0ce7dd0cb7
Neil John C. Basilisco <neilbasilisco158@gmail.com>
2024-10-16 10:07:31 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BASILISCO_ACTIVITY_IV.java
@@ -0,0 +1,37 @@
+class BASILISCO_ACTIVITY_IV {
+    public static void main(String[] args) {
+    
+        String firstname = "Neil John";
+        String lastname = " Basilisco ";            
+        String fullname = firstname + lastname;
+        int age = 18;
+        double height = 162.52;
+        char initial = 'C';
+        boolean isOpen = true;
+
+        String combine = fullname + age;
+    
+        
+        int x = 20;
+        int y = 10;
+        int z = 5;
+
+        int sum = x + y;
+        int diff = y - z;
+        int prod = x * z;
+        int quot = x / y;
+
+        System.out.println(combine);
+        System.out.println(fullname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(initial);
+        System.out.println(isOpen);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    
+    
+    }
+}
