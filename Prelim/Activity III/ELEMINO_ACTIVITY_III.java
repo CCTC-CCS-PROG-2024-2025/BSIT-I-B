6c3d33b5e66d617cd52206dace390331d3fca8a1
Jemima T. Elemino <maijem840@gmail.com>
2024-10-16 10:10:55 +0800

    Add files via upload

---
+++ Prelim/Activity III/ELEMINO_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class Elemino_ACTIVITY_III {
+  Public static void main (String[]args ) {
+   String name = "Jemima";
+   int age = 18;
+   double weight = 32;
+   float height = 1.45f;
+   char letter = 'J';
+   System.out.println(name);
+   System.out.println(age);
+   System.out.println(weigth);
+   System.out.println(height);
+   System.out.println(letter);
+}
+
+}
