e0f26fb415f2fe305123670ae97cf46aa54fed9a
Jave G. Sedillo <javesedillo30@gmail.com>
2024-10-16 10:34:33 +0800

    Add files via upload

---
+++ Prelim/Activity VI/SEDILLO_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class SEDILLO_ACTIVITY_VI {
+     public static void main(String[] args){
+        Scanner jave = new Scanner(System.in);
+        System.out.println(x:"Enter the first number");
+        int d = jave.nextInt();
+        System.out.println(x:"Enter the first number");
+        int e = jave.nextInt();
+
+        int sum = d + e;
+        int diff = d - e;
+        int prod = d * e;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+    } 
+}
