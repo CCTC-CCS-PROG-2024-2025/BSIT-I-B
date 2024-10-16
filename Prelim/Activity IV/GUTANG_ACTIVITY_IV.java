42fc5ba16d48ed9f844b937b2a565c1d04261965
Romeo B. Gutang <romeogutang20@gmail.com>
2024-10-16 10:11:19 +0800

    Add files via upload

---
+++ Prelim/Activity IV/GUTANG_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+public class GUTANG_ACTIVITY_IV {
+    public static void main (String[] args) {
+        String firstname = "Meok";
+        String lastname = "Gutang";
+
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(fullname);
+        int a = 20;
+        int e = 5;
+        int i = 2;
+        int sum = a+e;
+        int difference = i-e;
+        int product = a*e;
+        int quotient = e/i;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+
+    }
+}
