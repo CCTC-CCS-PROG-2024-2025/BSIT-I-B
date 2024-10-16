7ed153ac426ff00b27c7d19e3a4af9e2e243ad26
Yeshia P. Cabatingan <xwoofy06@gmail.com>
2024-10-16 10:08:42 +0800

    Add files via upload

---
+++ Prelim/Activity III/CABATINGAN_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+    class CABATINGAN_ACTIVITY_III {
+        public static void main(String[] args) {
+        String name = "Yeshia";
+        int age = 18;
+        double height = 1.5;
+        char letter = 'C';
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+        }
+     }
