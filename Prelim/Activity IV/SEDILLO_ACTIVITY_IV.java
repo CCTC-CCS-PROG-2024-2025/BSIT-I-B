fdc7de11cbafd80d223331cd352bc2c8740058a8
Jave G. Sedillo <javesedillo30@gmail.com>
2024-10-16 10:14:29 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SEDILLO_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+public class SEDILLO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        String firstname = "Jave";
+        String lastname = "Sedillo";
+
+        String fullname =  firstname + " " + lastname;
+
+        System.out.println(fullname);
+        int a = 20;
+        int b = 5;
+        int c = 2;
+        int sum = a+b;
+        int difference = b-c;
+        int product = a*c;
+        int quotient = b/c;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
+
