8f3f7a90034925f0cd7e87f1dd7bbed4c868edd3
Kiven F. Aganan <kivenaganan5@gmail.com>
2024-10-16 10:12:26 +0800

    Add files via upload

---
+++ Prelim/Activity IV/AGANAN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class AGANAN_ACTIVITY_IV {
+    public static void main(String[]args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum= a+b;
+        int diff= b-c;
+        int proud= a*c;
+        int quot= b/c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(proud);
+        System.out.println(quot);
+    }
+}
