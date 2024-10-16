c1fe523a47f645dc7a605e1e2557bd4239c12a39
Christian DAE M. Villaro <akamekyoto157@gmail.com>
2024-10-16 10:14:41 +0800

    Add files via upload

---
+++ Prelim/Activity III/Villaro_Activity_III.java
@@ -0,0 +1,16 @@
+class Villaro_Activity_III {
+    public static void main (String[] args) {
+        String Name = "Chris";
+        float height = 5.5f;
+        int num = 14;
+        double lenght = 153.6;
+        char Letter = 'C';
+
+        System.out.println(Name);
+        System.out.println(height);
+        System.out.println(num);
+        System.out.println(lenght);
+        System.out.println(Letter);
+
+    }
+}
