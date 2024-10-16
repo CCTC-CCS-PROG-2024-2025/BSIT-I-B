cd297951baedc33c9b9abe5a0bff38ab1a760c5e
Vanjo Telmoso <prealdiza@gmail.com>
2024-10-16 10:08:15 +0800

    Add files via upload

---
+++ Prelim/Activity III/TELMOSO_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+public class TELMOSO_ACTIVITY_III {
+    public static void main (String[]args){
+        String name = "VANJO";
+        int age = 21;
+        double height = 172.72;
+        float weight = 54f;
+        char letter = 'K';
+        boolean isStudent = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        System.out.println(isStudent);
+    }
+    }
+   
