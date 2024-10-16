bae9d5674baeec772f08b00536d006285e54caef
Paul Angelo A. Magbanua <paulangelomagbanua1@gmail.com>
2024-10-16 10:08:38 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MAGBANUA_ACTIVITY_IV.java
@@ -0,0 +1,27 @@
+class MAGBANUA_ACTIVITY_IV {
+public static void main (String[] args) {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    
+    int sum = a + b;
+    int diff = b - c;
+    int quot = c / a;
+    int prod = b * c;
+    String firstname = "Paul Angelo";
+    String lastname =  " Magbanua"; 
+    String fullname = firstname + lastname;
+    double height = 165;
+    double weight = 74;
+    
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(quot);
+    System.out.println(prod);
+    System.out.println(fullname);
+    System.out.println(height);
+    System.out.println(weight);
+
+}
+}
+
