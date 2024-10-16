4f96ca1141855c0e168b9b9a7c3f4e32a9bffd8e
Yeshia P. Cabatingan <xwoofy06@gmail.com>
2024-10-16 10:10:11 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CABATINGAN_ACTIVITY_IV.java
@@ -0,0 +1,40 @@
+public class CABATINGAN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String firstname = "Yeshia";
+        String lastname = "Cabatingan";
+        String fullname = firstname + lastname;
+        String color = "Red";
+
+        System.out.println(color);
+        System.out.println(fullname);
+
+        int x = 3;
+        int y = 6;
+        int z = 9;
+
+        int sum = x + y;
+        int diff = y + z;
+        int prod = z * x;
+        int quot = z / y;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+        double pricea = 15.99;
+        double priceb = 6.55;
+        double pricec = 5.00;
+        double priced = 7.10;
+
+        double plus = pricea + priceb;
+        double minus = priceb - pricec;
+        double times = pricec * priced;
+        double divide = pricea / pricec;
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(times);
+        System.out.println(divide);
+    }
+}
