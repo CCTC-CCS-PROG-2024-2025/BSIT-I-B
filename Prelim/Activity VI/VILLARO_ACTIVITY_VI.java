d37024ac5ccaf14b6cd93350131955f2d8570b25
Christian DAE M. Villaro <akamekyoto157@gmail.com>
2024-10-16 10:33:45 +0800

    Add files via upload

---
+++ Prelim/Activity VI/VILLARO_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Villaro_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int measure = sc.nextInt();
+        System.out.println("Enter the second number");
+        int height = sc.nextInt();
+    
+        System.out.println(measure);
+        System.out.println(height);
+
+    }
+
+      
+}
+   
+
+    
+  
