bb3071ec798264fd230185dde1352cb6eb4bfbfd
Marjun B. Lumangtad <marjunlumangtad49@gmail.com>
2024-10-16 10:07:29 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LUMANGTAD_ACTIVITY_IV.java
@@ -0,0 +1,30 @@
+public class LUMANGTAD_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 18;
+        int c = 19;
+
+        int pogi = a + b;
+        int pre = a - c;
+        int handsome = b * a;
+        int marjun = b / c;
+
+        String firstname = "Marjun";
+        String lastname = "Lumangtad";
+        String fullname = lastname + " " + firstname;
+        int age = 18;
+        double height = 162.69;
+        char letter = 'M';
+        Boolean isOpen = true;
+
+        System.out.println(pogi);
+        System.out.println(pre);
+        System.out.println(handsome);
+        System.out.println(marjun);
+        System.out.println(fullname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(isOpen);
+    }
+}
