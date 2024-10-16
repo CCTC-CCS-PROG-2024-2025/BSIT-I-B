d572cf90fb49a085111c2a431e8839a8aff2b8dd
Justine Mae Canete <justinemaecanete206@gmail.com>
2024-10-16 10:09:55 +0800

    Add files via upload

---
+++ Prelim/Activity III/CAÑETE_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class CAÑETE_ACTIVITY_III {
+    public static void main (String[]args ){
+        String name = "Justine";
+        int age = 18;
+        double weigth = 45 ;
+        float heigth = 1.45f;
+        char letter = 'J';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weigth);
+        System.out.println(heigth);
+        System.out.println(letter);
+
+    }
+}
