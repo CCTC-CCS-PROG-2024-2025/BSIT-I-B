206cb3c488c632dba017cdde4e9d35a95967148d
Bencent C. Sabulleros <bencesab05@gmail.com>
2024-12-11 09:47:58 +0800

    Add files via upload

---
+++ Midterm/Activity IV/SABULLEROS_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+
+class SABULLEROS_MIDTERM_EXAMINATION {
+
+    public static void main(String[]args){
+         Scanner sc = new Scanner (System.in);
+         System.out.println("enter a number to get the factorial");
+         int num = sc.nextInt();
+
+         int factorial = 1;
+         
+         for (Int i = 1; 1 <= num; i++) {
+            factorial *= 1;
+         }
+         System.out.println(factorial);
+    }
+}
