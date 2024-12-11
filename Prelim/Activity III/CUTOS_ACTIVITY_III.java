98e7ecb06385e0680f1641f614f30b269efb1e58
Dommy5 <paolocutos53@gmail.com>
2024-12-11 09:36:27 +0800

    Add files via upload

---
+++ Prelim/Activity III/CUTOS_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class CUTOS_ACTIVITY_III {
+public static void main(String[] args) {
+    String Name = "Paolo";
+    int age = 20;
+    double height = 160.45;
+    char letter = 'P';
+    boolean Isopen = true;
+
+    System.out.println(Name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(height);
+    System.out.println(letter);
+    System.out.println(Isopen);
+
+}
+}
