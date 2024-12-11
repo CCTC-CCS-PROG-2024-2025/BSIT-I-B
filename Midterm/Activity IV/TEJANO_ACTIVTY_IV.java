ade987373053f64992cc0a9ff038ce8af25f5339
Fritz A j Abendan Tejano <fritzaj19@gmail.com>
2024-12-11 20:48:49 +0800

    Add files via upload

---
+++ Midterm/Activity IV/TEJANO_ACTIVTY_IV.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TEJANO_ACTIVITY{
+
+
+
+
+    public static void main (String[]args){
+        Scanner scanner=new Scanner(Syestem.in);
+        Syestem.out.println(x:"Enter a non integer: ");
+        int num=scanner.nextint();
+
+
+        int fatorial=1;
+        for (int i=i;i<=num;i++){
+            factorial*=i;
+        }
+        Syestem.out.println("The factorial of " + num + "is" + factorial);
+        
+    }
+}
