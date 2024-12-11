0b4c2cd4569f7f546f9307053446398f0e208532
Paul Angelo A. Magbanua <paulangelomagbanua1@gmail.com>
2024-12-11 09:17:40 +0800

    Add files via upload

---
+++ Midterm/Activity IV/MAGBANUA_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class MAGBANUA_ACTIVITY_IV{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a non-negative integer: ");
+
+        int nni = sc.nextInt() ;
+        
+        if (nni < 0){
+            System.out.println("Please Enter a non-negative integer: ");
+          } else {
+            int factorial = 1;
+
+            for (int i = 1; i <= nni; i++ ){
+                factorial *= i;
+            }
+          System.out.println("The factorial of " + nni + " is: " + factorial);
+        }
+    }
+}
