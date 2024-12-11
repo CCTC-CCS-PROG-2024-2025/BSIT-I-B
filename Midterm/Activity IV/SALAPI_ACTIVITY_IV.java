408ec10e5c955972e0d218a1e803c6fb19fec3ed
Dean Mark A. Salapi <salapidean@gmail.com>
2024-12-11 09:01:55 +0800

    Add files via upload

---
+++ Midterm/Activity IV/SALAPI_ACTIVITY_IV.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class SALAPI_ACTIVITY_IV{
+
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter a non-negative integer: ");
+
+        int nni = sc.nextInt();
+        //nni = non-negative integer
+
+        if (nni < 0){
+            System.out.println("Please Enter a non-negative integer. ");
+         } else {
+            int factorial = 1;
+
+            for(int i = 1; i <= nni; i++ ){
+                factorial *= i;
+
+            }
+
+            System.out.println("The Factorial of " + nni +"is: " + factorial);
+        }
+
+
+    }
+}
