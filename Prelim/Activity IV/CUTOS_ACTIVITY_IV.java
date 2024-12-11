14601152e12c2e53ef2458913e8627f47bf0266d
Dommy5 <paolocutos53@gmail.com>
2024-12-11 09:40:13 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CUTOS_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+class CUTOS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String Firstname = "Paolo louis";
+        String lastname = "CUTOS";
+
+
+        int Age = 20;
+        char Gender ='M';
+        double Height =160.45 
+
+        String Mix = Fullname + Age;
+
+        int P = 31;
+        int L = 29;
+        int C = 18;
+
+        int Sum = P + L;
+        int Diff = C - L;
+        int prod = L * C;
+        int quot = C / P;
+
+        boolean
+
+
+
+
+
+    }
+}
