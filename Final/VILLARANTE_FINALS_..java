5117cdf9285d423877207bfd1d4beca77865c551
Michelle Villarante <villarantemichelle35@gmail.com>
2024-12-23 19:31:42 +0800

    Add files via upload

---
+++ Final/VILLARANTE_FINALS_..java
@@ -0,0 +1,97 @@
+import java.util.Scanner ;
+    class VILLARANTE_FINAL{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int a = 5;
+        int b = 4;
+        int c = 2;
+
+        int sum = a + b;
+        int diff = a - c;
+        int pro = b * c;
+        
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(pro);
+        
+
+        String firstname = "Michelle";
+        String lastname = "Villarante";
+        int age = 19;
+        double hight = 5.1;
+        float wight = 49.6f;
+        boolean isfemale = true;
+        char bloodtype = 'A';
+
+        System.out.println(lastname + lastname);
+        System.out.println(hight + wight);
+        System.out.println(wight - hight);
+        System.out.println(isfemale);
+        System.out.println(bloodtype);
+       
+   
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+           
+       
+            default:
+                System.out.println("Invalid day");
+       
+
+         int score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else if (score >= 70) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: F");
+            
+            
+            
+         int x = 2;
+           while (x<7) {
+               System.out.println(x);
+               x++;
+          
+             
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        
+        }
+    }
+}
+
+   }
+     }   
+   
+}       
+
+
+  
+            
+    
+        
+    
+       
+        
+        
+ 
+    
+    
+       
+         
+           
+ 
