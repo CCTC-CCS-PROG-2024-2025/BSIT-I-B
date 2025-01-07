e0e0403ca53fc79da070c0775b90efa2ffe5895a
pardzz <Joselitopardillo2005@gmail.com>
2025-01-07 11:13:32 +0800

    Add files via upload

--- Prelim/Activity III/PARDILLO_ACTIVITY_III.java
+++ Prelim/Activity III/PARDILLO_ACTIVITY_III.java
@@ -1,17 +1,21 @@
-class SIGFRED_ACTIVITY_III {
-    public static void main(String[] args) {
-        String name = "SIGFRED";
-        int age = 21;
-        double height = 72;
-        float weight = 62f;
-        char letter = 'S';
-        boolean isStudent = true;
+class PARDILLO_ACTIVITY_III {
+
+
+
+    public static void main (String [] args) {
+        String name = "PARDILLO";
+        int age = 18;
+        double height = 1.6;
+        char letter = 'A' ;
+        boolean isOpen = true;
+
         System.out.println(name);
         System.out.println(age);
         System.out.println(height);
-        System.out.println(weight);
-        System.out.println(isStudent);
         System.out.println(letter);
-    }    
-}
+        System.out.println(isOpen);
 
+    
+        
+    }
+}   
