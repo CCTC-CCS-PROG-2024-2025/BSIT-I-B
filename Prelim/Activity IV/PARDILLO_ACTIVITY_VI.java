b9c45cd3d3129749ca6283965c7d747ed0f00f21
pardzz <Joselitopardillo2005@gmail.com>
2025-01-07 11:15:07 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PARDILLO_ACTIVITY_VI.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+
+ class PARDILLO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(x:"Enter the first number:");
+        int num5 = sc.nextInt();
+        System.out.println(x:"Enter the first number:");
+        int num6 = sc.nextInt();
+
+        int sum = num5 + num6;
+        int diff = num5 + num6;
+        int prod = num5 + num6;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+
+    }
+}
