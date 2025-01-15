0ff559960a8bd0f7ace58099b3ca9d00aff32e27
Yeshia P. Cabatingan <xwoofy06@gmail.com>
2025-01-15 11:17:32 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/CABATINGAN_ACTIVITY_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class CABATINGAN_ACTIVITY_1 {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        String [] names = new String [5];
+
+        //Insert
+        for (int t = 0; t <= 4; i++) {
+            System.out.println ("Insert Name" + (t + 1));
+            names [t] = scanner.nextLine();
+        }
+        //Enter a number
+        System.out.println("Enter another number:");
+        int num = scanner.nextInt();
+        System.out.println(names [num-1]);
+    }
+}
