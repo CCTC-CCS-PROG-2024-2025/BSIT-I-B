ffa4e055d92612608b94e7070c48f5b8b2ea876e
Jann Paul C. Casia <jannpaulcasia2@gmail.com>
2025-01-15 10:19:53 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/CASIA_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class CASIA_ACTIVITY_I{
+
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        String[] names =new String[5];
+        for (int i = 0; i <= 4; i++) {
+            System.out.print("Enter name " + (i + 1) );
+            names[i] = sc.nextLine();
+        }
+
+        // Ask the user to enter a number to display a name
+        System.out.println("Enter a number (1-5) corresponding to the name you want to display: ");
+        int number= sc.nextInt();
+
+        System.out.println(names[number-1]);
+
+    }
+}
