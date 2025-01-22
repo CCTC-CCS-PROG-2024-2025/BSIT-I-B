c5c6dc51819b4fa6ba39f59ee53acaf761a60580
Czelle Adrienne Gesite <Pueblosoliver050105@gmail.com>
2025-01-22 09:15:58 +0800

    Update GESITE_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/GESITE_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/GESITE_ACTIVITY_I.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+class GESITE_ACTIVITY_I{
 
-public class GESITE_ACTIVITY_I {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] names = new String[5];
-        System.out.println("Enter 5 names:");
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        String[] names =new String[5];
         for (int i = 0; i <= 4; i++) {
-            System.out.print("Enter name " + (i + 1) + ": ");
-            names[i] = scanner.nextLine();
-        }
-        System.out.print("Enter a number between 1 and 5 to display a name: ");
-        int index = scanner.nextInt();
-        if (index >= 1 && index <= 5) {
-            System.out.println("The name at position " + index + " is: " + names[index - 1]);
-        } else {
-            System.out.println("Please enter a number between 1 and 5.");
+            System.out.print("Enter name " + (i + 1) );
+            names[i] = sc.nextLine();
         }
 
-        
+        // Ask the user to enter a number to display a name
+        System.out.println("Enter a number (1-5) corresponding to the name you want to display: ");
+        int number= sc.nextInt();
+
+        System.out.println(names[number-1]);
+
     }
 }
