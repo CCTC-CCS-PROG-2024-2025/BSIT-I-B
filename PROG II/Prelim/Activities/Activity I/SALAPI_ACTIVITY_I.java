491bea450b786701f88cc94496f5dfb989338106
Dean Mark A. Salapi <salapidean@gmail.com>
2025-01-22 09:07:50 +0800

    Update SALAPI_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/SALAPI_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/SALAPI_ACTIVITY_I.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-class SALAPI_ACTIVITY_I{
+
+public class SALAPI_ACTIVITY_I {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] name = new String [5];
-     
-        System.out.println("ENTER YOUR NAME");
-
-        for ( int x = 0; x < name.length; x++) {
-            System.out.println("Enter name" + " : ");
-
-            name [x] = sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[5];
+        System.out.println("Enter 5 names:");
+        for (int i = 0; i <= 4; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+        System.out.print("Enter a number between 1 and 5 to display a name: ");
+        int index = scanner.nextInt();
+        if (index >= 1 && index <= 5) {
+            System.out.println("The name at position " + index + " is: " + names[index - 1]);
+        } else {
+            System.out.println("Please enter a number between 1 and 5.");
         }
 
-        System.out.println("YOU ENTERED THE FOLLOWING NAMES:");
-        for (int x = 0; x < name.length; x++) {
-            System.out.println((x + 1) + ". " + name[x]);
-        }
         
-     }
+    }
 }
