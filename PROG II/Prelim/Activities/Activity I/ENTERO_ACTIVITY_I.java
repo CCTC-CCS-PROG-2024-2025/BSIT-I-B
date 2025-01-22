1611385a1c57bad1eafa1e1711d181ed91f1c816
Earl John H. Entero <earlentero@gmail.com>
2025-01-22 09:10:18 +0800

    Update ENTERO_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/ENTERO_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/ENTERO_ACTIVITY_I.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-class ENTERO_ACTIVITY_I {
+
+public class ENTERO_ACTIVITY_I {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] name = new String [5];
-     
-        System.out.println("ENTER YOUR NAME");
-
-        for ( int x = 0; x < name.length; x++) {
-            System.out.println("Enter name" + " : ");
-
-            name [x] = sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[5];
+        System.out.println("ENTER ATLEAST 5 NAMES:");
+        for (int i = 0; i <= 4; i++) {
+            System.out.print("ENTER NAME " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+        System.out.print("ENTER A NUMBER FROM 1-5: ");
+        int index = scanner.nextInt();
+        if (index >= 1 && index <= 5) {
+            System.out.println("POSITION " + index + " is: " + names[index - 1]);
+        } else {
+            System.out.println("ENTER A NUMBER FROM 1-5.");
         }
 
-        System.out.println("YOU ENTERED THE NAME: ");
-        for (int x = 0; x < name.length; x++) {
-            System.out.println((x + 1) + ". " + name[x]);
-        }
         
-     }
+    }
 }
