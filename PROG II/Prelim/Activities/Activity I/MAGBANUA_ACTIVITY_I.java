4156c415b775467db19050611231e1110d08630d
Paul Angelo A. Magbanua <paulangelomagbanua1@gmail.com>
2025-01-24 21:41:27 +0800

    MAGBANUA_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/MAGBANUA_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/MAGBANUA_ACTIVITY_I.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
+
 class MAGBANUA_ACTIVITY_I {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] name = new String [5];
-     
-        System.out.println("Enter your Desired Name: ");
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[5];
 
-        for ( int x = 0; x < name.length; x++) {
-            System.out.println("Enter name" + " : ");
-
-            name [x] = sc.nextLine();
-        }
-
-        System.out.println("You entered a name: ");
-        for (int x = 0; x < name.length; x++) {
-            System.out.println((x + 1) + ". " + name[x]);
-        }
         
-     }
+        System.out.println("Please Enter Five Names:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+
+        
+        System.out.println("Enter a Number Between 1 to 5 to Display a Name:");
+        int index = scanner.nextInt();
+
+        
+        if (index >= 1 && index <= 5) {
+            System.out.println("The Name at Position " + index + " is: " + names[index - 1]);
+        } else {
+            System.out.println("Please Enter a Number Between 1 and 5.");
+        }
+
+        
+    }
 }
