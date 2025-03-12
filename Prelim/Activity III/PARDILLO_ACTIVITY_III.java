85bfdf27cbf64aef983bfa30d71a44429f7528c1
Sigfred O. Pardillo <pardillosigfred@gmail.com>
2025-03-12 08:53:04 +0800

    Add files via upload

--- Prelim/Activity III/PARDILLO_ACTIVITY_III.java
+++ Prelim/Activity III/PARDILLO_ACTIVITY_III.java
@@ -1,21 +1,17 @@
-class PARDILLO_ACTIVITY_III {
-
-
-
-    public static void main (String [] args) {
-        String name = "PARDILLO";
-        int age = 18;
-        double height = 1.6;
-        char letter = 'A' ;
-        boolean isOpen = true;
-
+public class NARVASA_ACTIVITY_III {
+    public static void main (String[]args){
+        String name = "VANJO";
+        int age = 21;
+        double height = 172.72;
+        float weight = 54f;
+        char letter = 'K';
+        boolean isStudent = true;
         System.out.println(name);
         System.out.println(age);
         System.out.println(height);
+        System.out.println(weight);
         System.out.println(letter);
-        System.out.println(isOpen);
-
-    
-        
+        System.out.println(isStudent);
     }
-}   
+    }
+   
