ecd2a4c2638a18150868ac76c94c41664ee90e8f
pardzz <Joselitopardillo2005@gmail.com>
2025-03-12 11:08:36 +0800

    Update PARDILLO_ACTIVITY_IV.java

--- Midterm/Activity IV/PARDILLO_ACTIVITY_IV.java
+++ Midterm/Activity IV/PARDILLO_ACTIVITY_IV.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class GESITE_ACTIVITY_IV{
+class PARDILLO_ACTIVITY_IV{
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
         System.out.println("enter a non negative integer");
