36003eb23374d1e53c715227ef4a885e77e4c295
Stephanie Ninal <steffyninal23@gmail.com>
2025-03-12 09:55:56 +0800

    Add files via upload

---
+++ Prelim/Activity VI/NIÑAL_ACTIVITY_VI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NIÑAL_ACTIVITY_VI {
+    public static void main (String [] args){
+        Scanner = new Scanner (System.in);
+        System.out.println("Enter your first letter:");
+        String balls = sc.nextLine();
+        
+        System.out.println("Enter your first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter your second number:");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 + num2;
+        int prod = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+    }
+}
