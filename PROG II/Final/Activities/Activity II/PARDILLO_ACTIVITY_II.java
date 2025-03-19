8c9513586786fa8510f3ae24a731f70873a788c5
Sigfred O. Pardillo <pardillosigfred@gmail.com>
2025-03-19 09:54:36 +0800

    Add files via upload

--- PROG II/Final/Activities/Activity II/PARDILLO_ACTIVITY_II.java
+++ PROG II/Final/Activities/Activity II/PARDILLO_ACTIVITY_II.java
@@ -1,22 +1,14 @@
 import java.util.Scanner;
-
-public class FeetToInchesConverter {
-    // Method to convert feet to inches
-    public static int convertToInches(int feet) {
+public class PARDILLO_ACTIVITY_II{
+    public static int convertToInches(int feet){
         return feet * 12;
     }
+        public static void main(String[]args){
+            Scanner sc =new Scanner(System.in);
+            System.out.println("Enter the number of feet");
+            int feet=sc.nextInt();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for input
-        System.out.print("Enter the number of feet: ");
-        int feet = scanner.nextInt();
-
-        // Convert and display result
-        int inches = convertToInches(feet);
-        System.out.println("Inches: " + inches);
-
-        scanner.close();
+            int inches=convertToInches(feet);
+            System.out.println("Inches:" +inches);
+        }
     }
-}
