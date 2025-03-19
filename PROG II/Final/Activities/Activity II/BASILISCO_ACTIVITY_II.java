fd2a6e6c0a18a7d0a93affa228918c07231d49ba
Neil John C. Basilisco <neilbasilisco158@gmail.com>
2025-03-19 09:45:13 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/BASILISCO_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BASILISCO_ACTIVITY_II {
+    
+    public static int convertToInches(int feet) {
+        return feet * 12;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter feet: ");
+        int feet = scanner.nextInt();
+        
+        
+        int inches = convertToInches(feet);
+        System.out.println("Inches: " + inches);
+        
+        
+        scanner.close();
+    }
+}
