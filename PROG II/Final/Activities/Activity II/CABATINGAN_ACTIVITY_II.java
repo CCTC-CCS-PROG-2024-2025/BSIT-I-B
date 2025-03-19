a89c0168f7c9a27399ece08208cdc93ed82b3c4d
Yeshia P. Cabatingan <xwoofy06@gmail.com>
2025-03-19 09:47:43 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/CABATINGAN_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class CABATINGAN_ACTIVITY_II {
+    public static int FeettoInchesConverter (int feet) {
+        return feet *12;
+    }
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert feet");
+        int feet = sc.nextInt();
+
+        int inches = FeettoInchesConverter(feet);
+        System.out.println("Inches: " + inches);
+        
+        sc.close ();
+    }
+}
