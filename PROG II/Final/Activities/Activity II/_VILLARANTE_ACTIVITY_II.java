a785dd343402eb0a30232c390569c2fd8eb4826e
Michelle Villarante <villarantemichelle35@gmail.com>
2025-03-19 09:41:12 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/_VILLARANTE_ACTIVITY_II.java
@@ -0,0 +1,50 @@
+import java.util.Scanner
+public class _VILLARANTE_II{
+    public static int 
+    convertToInches(int feet){
+        return feet*12;
+ }
+public static void main(String []args){
+    Scanner Sc = new Scanner(System.in)
+
+System.out.println("Enter the feet");
+int feet = scanner .nextInt();
+int inches =convertToInches(feet);
+System.out.println("Inches:"+ inches);
+scanner.close();
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
