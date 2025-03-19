3e241f80c6a7ead016ce389d71ac518c6cd18f68
Vanjo Telmoso <prealdiza@gmail.com>
2025-03-19 09:50:59 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/TELMOSO_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TELMOSO_ACTIVITY_II {
+   static void double convertToDouble(double feet){
+    return feet * 12;
+   }
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number in feet: ");
+    double feet = sc.nextDouble();
+
+    double inches = convertToDouble(feet);
+
+    System.out.println(feet + " feet is equal to " + inches + " inches.");
+    scanner.close();
+   }
+}
+
+
