1c09ffc6c0242cb4bbc005ddab37f6a984aed1a3
Audey Amir C. Agbay <audeyamirjoestar@gmail.com>
2025-03-19 09:48:57 +0800

    AGBAY_ACTIVITY_II.java

--- PROG II/Final/Activities/Activity II/AGBAY_ACTIVITY_II.java
+++ PROG II/Final/Activities/Activity II/AGBAY_ACTIVITY_II.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-
 public class AGBAY_ACTIVITY_II {
-   static void double convertToDouble(double feet){
-    return feet * 12;
-   }
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static int convertToInches(int feet){
+     return feet*12;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of feet");
+    int feet = sc.nextInt();
 
-    System.out.println("Enter a number in feet: ");
-    double feet = sc.nextDouble();
+    int inches = convertToInches(feet);
+    System.out.println("Inches: " + inches);
 
-    double inches = convertToDouble(feet);
+    }
 
-    System.out.println(feet + " feet is equal to " + inches + " inches.");
-    scanner.close();
-   }
+    
 }
 
 
