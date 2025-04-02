4940f85f1d878c1320a70c16279c111fdec02e74
Yeshia P. Cabatingan <xwoofy06@gmail.com>
2025-04-02 09:49:11 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity III/CABATINGAN_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class CABATINGAN_ACTIVITY_III {
+    public static double ConvertToFahrenheit(int celsius){
+        return(celsius*9.0/5) +32;
+    }
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Temperature in Celsius: ");
+        int celsius = scanner.nextInt();
+        double fahrenheit = ConvertToFahrenheit(celsius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+        
+        scanner.close();
+    }
+}
