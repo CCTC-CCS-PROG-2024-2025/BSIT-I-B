2b4fac27635f738c790ef3e3d7918d32b8110cc9
Earl John H. Entero <earlentero@gmail.com>
2025-04-02 09:48:21 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity III/ENTERO_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ENTERO_ACTIVITY_III {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+        
+        double fahrenheit = celsiusToFahrenheit(celsius);
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+
+        sc.close();
+    }
+}
