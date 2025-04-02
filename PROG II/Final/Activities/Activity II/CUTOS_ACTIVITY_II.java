1038023a97d04bb5e9305bfebd8a1481f6c69943
Cutos Paolo Louis <louiskiecapote@gmail.com>
2025-04-02 10:17:28 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/CUTOS_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CUTOS_ACTIVITY_II{
+    public static int convertToInches(int feet) {
+        return feet * 12;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter feet: ");
+        int feet = scanner.nextInt();
+        
+        int inches = convertToInches(feet);
+        System.out.println("Inches: " + inches);
+        
+        scanner.close();
+    }
+}
