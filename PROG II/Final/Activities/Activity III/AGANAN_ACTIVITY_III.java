5d94efd374a7ac8addc2465343369f5c04c3fbea
Kiven F. Aganan <kivenaganan5@gmail.com>
2025-04-02 09:52:06 +0800

    Create AGANAN_ACTIVITY_III.java

---
+++ PROG II/Final/Activities/Activity III/AGANAN_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AGANAN_ACTIVITY_III {
+
+    public static double convertCelsiusToFahrenheit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+
+        double fahrenheit = convertCelsiusToFahrenheit();
+
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
