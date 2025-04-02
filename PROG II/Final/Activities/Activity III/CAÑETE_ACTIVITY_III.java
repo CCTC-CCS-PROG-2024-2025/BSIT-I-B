bc324cf643c6b90db82723a030daca951567ede2
Justine Mae Cañete <justinemaecanete206@gmail.com>
2025-04-02 09:41:37 +0800

    Update CAÑETE_ACTIVITY_III.java

--- PROG II/Final/Activities/Activity III/CAÑETE_ACTIVITY_III.java
+++ PROG II/Final/Activities/Activity III/CAÑETE_ACTIVITY_III.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CAÑETE_Activity_III {
     
-    // Method to convert Celsius to Fahrenheit
+
     public static double convertToFahrenheit(int celsius) {
         return (celsius * 9.0 / 5) + 32;
     }
@@ -10,14 +10,11 @@ public class CAÑETE_Activity_III {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Prompt user for input
         System.out.print("Enter temperature in Celsius: ");
         int celsius = scanner.nextInt();
         
-        // Call the conversion method
         double fahrenheit = convertToFahrenheit(celsius);
         
-        // Display the result
         System.out.println("Fahrenheit: " + fahrenheit);
         
         scanner.close();
